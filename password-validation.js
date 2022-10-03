220655f89d6852349da307ccbf2efb740e1afd3c
Brian Liao <yaramiu@gmail.com>
2022-10-03 15:29:24 -0400

    Implement password validation

--- password-validation.js
+++ password-validation.js
@@ -1,1 +1,39 @@
+const passwordElement = document.getElementById("password");
+const confirmPasswordElement = document.getElementById("confirm-password");
+const span = document.querySelector(".password-match");
 
+const submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("click", () => {
+  const isMatchingPasswords = checkPasswordsMatch();
+  if (!isMatchingPasswords) {
+    addErrorStyles();
+  } else {
+    removeErrorStyles();
+  }
+});
+
+function checkPasswordsMatch() {
+  return passwordElement.value === confirmPasswordElement.value;
+}
+
+function removeErrorStyles() {
+  passwordElement.classList.remove("error");
+  confirmPasswordElement.classList.remove("error");
+
+  passwordElement.style.border = "2px solid #e5e7eb";
+  confirmPasswordElement.style.border = "2px solid #e5e7eb";
+
+  span.textContent = "";
+  span.style.color = black;
+}
+
+function addErrorStyles() {
+  passwordElement.classList.add("error");
+  confirmPasswordElement.classList.add("error");
+
+  passwordElement.style.border = "2px solid red";
+  confirmPasswordElement.style.border = "2px solid red";
+
+  span.textContent = "* Passwords do not match";
+  span.style.color = "red";
+}
